ce6ca0ecd48b9649cd6b00082f1605899abd4e6d
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2024-03-14 11:36:51 +0100

    tests: all tests passed

--- app/api/tests/conftest.py
+++ app/api/tests/conftest.py
@@ -7,6 +7,7 @@ from tortoise.connection import connections
 from app.api.utils.auth import Authentication
 import pytest, asyncio, os
 from app.db.models.chat import Chat, Message
+from app.db.models.feed import Comment, Post, Reaction, Reply
 from app.db.models.profiles import Friend
 from app.main import app
 
@@ -25,11 +26,11 @@ def event_loop():
 @pytest.fixture(scope="session")
 async def db_conf():
     NEW_ORM_CONF = TORTOISE_ORM
-    NEW_ORM_CONF["connections"]["default"] = "sqlite://test.db"
+    NEW_ORM_CONF["connections"]["default"] = "sqlite://:memory:"
     yield NEW_ORM_CONF
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture()
 async def setup_db(db_conf):
     await Tortoise.init(config=db_conf)
     await Tortoise.generate_schemas()
@@ -39,7 +40,7 @@ async def setup_db(db_conf):
 
 
 @pytest.fixture()
-async def client(mocker, db_conf):
+async def client(mocker, db_conf, setup_db):
     mocker.patch("app.db.config.TORTOISE_ORM", new=db_conf)
 
     async with AsyncTestClient(app=app) as client:
@@ -58,11 +59,11 @@ async def test_user():
         "email": "test@example.com",
         "password": "testpassword",
     }
-    user = await User.get_or_create(email=user_dict["email"], defaults=user_dict)
+    user = await User.create_user(user_dict)
     return user
 
 
-@pytest.fixture
+@pytest.fixture()
 async def verified_user():
     user_dict = {
         "first_name": "Test",
@@ -71,7 +72,7 @@ async def verified_user():
         "password": "testpassword",
         "is_email_verified": True,
     }
-    user = await User.get_or_create(email=user_dict["email"], defaults=user_dict)
+    user = await User.create_user(user_dict)
     return user
 
 
@@ -84,12 +85,12 @@ async def another_verified_user():
         "password": "anothertestverifieduser123",
         "is_email_verified": True,
     }
-    user = await User.get_or_create(email=user_dict["email"], defaults=user_dict)
+    user = await User.create_user(user_dict)
     return user
 
 
 @pytest.fixture
-async def authorized_client(verified_user: User, client: AsyncTestClient):
+async def authorized_client(client: AsyncTestClient, verified_user: User):
     verified_user.access_token = await Authentication.create_access_token(
         {"user_id": str(verified_user.id), "username": verified_user.username}
     )
@@ -103,20 +104,27 @@ async def authorized_client(verified_user: User, client: AsyncTestClient):
 
 
 @pytest.fixture
-async def another_authorized_client(
-    another_verified_user: User, client: AsyncTestClient
-):
-    another_verified_user.access_token = await Authentication.create_access_token(
+async def another_verified_user_tokens(another_verified_user: User):
+    access = await Authentication.create_access_token(
         {
             "user_id": str(another_verified_user.id),
             "username": another_verified_user.username,
         }
     )
-    another_verified_user.refresh_token = await Authentication.create_refresh_token()
+    refresh = await Authentication.create_refresh_token()
+    another_verified_user.access_token = access
+    another_verified_user.refresh_token = refresh
     await another_verified_user.save()
+    return {"access": access, "refresh": refresh}
+
+
+@pytest.fixture
+async def another_authorized_client(
+    client: AsyncTestClient, another_verified_user_tokens
+):
     client.headers = {
         **client.headers,
-        "Authorization": f"Bearer {another_verified_user.access_token}",
+        "Authorization": f"Bearer {another_verified_user_tokens['access']}",
     }
     return client
 
@@ -135,13 +143,11 @@ async def city():
 
 @pytest.fixture
 async def friend(verified_user: User, another_verified_user: User):
-    friend = await Friend.get_or_none(
-        requester=verified_user, requestee=another_verified_user
-    ).select_related("requester", "requestee")
-    if not friend:
-        friend = await Friend.create(
-            requester=verified_user, requestee=another_verified_user, status="ACCEPTED"
-        )
+    friend = await Friend.create(
+        status="ACCEPTED",
+        requester=verified_user,
+        requestee=another_verified_user,
+    )
     return friend
 
 
@@ -152,40 +158,67 @@ async def friend(verified_user: User, another_verified_user: User):
 @pytest.fixture
 async def chat(verified_user, another_verified_user):
     # Create Chat
-    chat = await Chat.get_or_none(ctype="DM").prefetch_related("owner", "users")
-    if not chat:
-        chat = await Chat.create(owner=verified_user)
-        await chat.users.add(another_verified_user)
+    chat = await Chat.create(ctype="DM", owner=verified_user)
+    await chat.users.add(another_verified_user)
     return chat
 
 
 @pytest.fixture
 async def group_chat(verified_user, another_verified_user):
     # Create Group Chat
-    chat = await Chat.get_or_none(
+    chat = await Chat.create(
         owner=verified_user,
         name="My New Group",
         ctype="GROUP",
         description="This is the description of my group chat",
-    ).prefetch_related("owner", "users")
-    if not chat:
-        chat = await Chat.create(
-            owner=verified_user,
-            name="My New Group",
-            ctype="GROUP",
-            description="This is the description of my group chat",
-        )
-        await chat.users.add(another_verified_user)
+    )
+    await chat.users.add(another_verified_user)
     return chat
 
 
 @pytest.fixture
 async def message(chat):
     # Create Message
-    message = await Message.get_or_none().select_related("chat", "sender")
-    if not message:
-        message = await Message.create(chat=chat, sender=chat.owner, text="Hello Boss")
+    message = await Message.create(chat=chat, sender=chat.owner, text="Hello Boss")
     return message
+
+
+# -------------------------------------------------------------------------------
+
+
+# FEED FIXTURES
+@pytest.fixture
+async def post(verified_user):
+    # Create Post
+    post = await Post.create(text="This is a nice new platform", author=verified_user)
+    return post
+
+
+@pytest.fixture
+async def reaction(post):
+    # Create Reaction
+    author = post.author
+    reaction = await Reaction.create(rtype="LIKE", user=author, post=post)
+    return reaction
+
+
+@pytest.fixture
+async def comment(post):
+    # Create Comment
+    author = post.author
+    comment = await Comment.create(text="Just a comment", author=author, post=post)
+    comment.reactions_count = 0
+    comment.replies_count = 0
+    return comment
+
+
+@pytest.fixture
+async def reply(comment):
+    # Create Reply
+    author = comment.author
+    reply = await Reply.create(text="Simple reply", author=author, comment=comment)
+    reply.reactions_count = 0
+    return reply
 
 
 # -------------------------------------------------------------------------------
---
+++ app/api/tests/test_feed.py
@@ -0,0 +1,485 @@
+from app.common.exception_handlers import ErrorCode
+import uuid
+
+BASE_URL_PATH = "/api/v5/feed"
+
+
+async def test_retrieve_posts(client, post, mocker):
+    response = await client.get(f"{BASE_URL_PATH}/posts")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Posts fetched",
+        "data": {
+            "per_page": 50,
+            "current_page": 1,
+            "last_page": 1,
+            "posts": [
+                {
+                    "author": mocker.ANY,
+                    "text": post.text,
+                    "slug": post.slug,
+                    "reactions_count": mocker.ANY,
+                    "comments_count": mocker.ANY,
+                    "image": None,
+                    "created_at": mocker.ANY,
+                    "updated_at": mocker.ANY,
+                }
+            ],
+        },
+    }
+
+
+async def test_create_post(authorized_client, mocker):
+    post_dict = {"text": "My new Post"}
+    response = await authorized_client.post(f"{BASE_URL_PATH}/posts", json=post_dict)
+    assert response.status_code == 201
+    assert response.json() == {
+        "status": "success",
+        "message": "Post created",
+        "data": {
+            "author": mocker.ANY,
+            "text": post_dict["text"],
+            "slug": mocker.ANY,
+            "reactions_count": 0,
+            "comments_count": 0,
+            "created_at": mocker.ANY,
+            "updated_at": mocker.ANY,
+            "file_upload_data": None,
+        },
+    }
+
+
+async def test_retrieve_post(client, post, mocker):
+    # Test for post with invalid slug
+    response = await client.get(f"{BASE_URL_PATH}/posts/invalid_slug")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "code": ErrorCode.NON_EXISTENT,
+        "message": "Post does not exist",
+    }
+
+    # Test for post with valid slug
+    response = await client.get(f"{BASE_URL_PATH}/posts/{post.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Post Detail fetched",
+        "data": {
+            "author": mocker.ANY,
+            "text": post.text,
+            "slug": post.slug,
+            "reactions_count": mocker.ANY,
+            "comments_count": mocker.ANY,
+            "image": None,
+            "created_at": mocker.ANY,
+            "updated_at": mocker.ANY,
+        },
+    }
+
+
+async def test_update_post(
+    authorized_client, another_verified_user_tokens, post, mocker
+):
+    post_dict = {"text": "Post Text Updated"}
+
+    # Check if endpoint fails for invalid post
+    response = await authorized_client.put(
+        f"{BASE_URL_PATH}/posts/invalid_slug", json=post_dict
+    )
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "code": ErrorCode.NON_EXISTENT,
+        "message": "Post does not exist",
+    }
+
+    # Check if endpoint fails for invalid owner
+    response = await authorized_client.put(
+        f"{BASE_URL_PATH}/posts/{post.slug}",
+        json=post_dict,
+        headers={"Authorization": f"Bearer {another_verified_user_tokens['access']}"},
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "status": "failure",
+        "code": ErrorCode.INVALID_OWNER,
+        "message": "This Post isn't yours",
+    }
+
+    # Check if endpoint succeeds if all requirements are met
+    response = await authorized_client.put(
+        f"{BASE_URL_PATH}/posts/{post.slug}", json=post_dict
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Post updated",
+        "data": {
+            "author": mocker.ANY,
+            "text": post_dict["text"],
+            "slug": mocker.ANY,
+            "reactions_count": 0,
+            "comments_count": 0,
+            "created_at": mocker.ANY,
+            "updated_at": mocker.ANY,
+            "file_upload_data": None,
+        },
+    }
+
+
+async def test_delete_post(authorized_client, post):
+    # Check if endpoint fails for invalid post
+    response = await authorized_client.delete(f"{BASE_URL_PATH}/posts/invalid_slug")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "code": ErrorCode.NON_EXISTENT,
+        "message": "Post does not exist",
+    }
+
+    # Check if endpoint succeeds if all requirements are met
+    response = await authorized_client.delete(f"{BASE_URL_PATH}/posts/{post.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Post deleted",
+    }
+
+
+async def test_retrieve_reactions(client, reaction):
+    author = reaction.user
+    post = reaction.post
+    # Test for invalid focus_value
+    response = await client.get(f"{BASE_URL_PATH}/reactions/invalid_focus/{post.slug}")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Invalid 'focus' value",
+        "code": ErrorCode.INVALID_VALUE,
+    }
+
+    # Test for invalid slug
+    response = await client.get(f"{BASE_URL_PATH}/reactions/POST/invalid_slug")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Post does not exist",
+        "code": ErrorCode.NON_EXISTENT,
+    }
+
+    # Test for valid values
+    response = await client.get(f"{BASE_URL_PATH}/reactions/POST/{post.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Reactions fetched",
+        "data": {
+            "per_page": 50,
+            "current_page": 1,
+            "last_page": 1,
+            "reactions": [
+                {
+                    "id": str(reaction.id),
+                    "user": {
+                        "name": author.full_name,
+                        "username": author.username,
+                        "avatar": author.get_avatar,
+                    },
+                    "rtype": reaction.rtype,
+                }
+            ],
+        },
+    }
+
+
+async def test_create_reaction(authorized_client, post, mocker):
+    user = post.author
+    reaction_dict = {"rtype": "LOVE"}
+    response = await authorized_client.post(
+        f"{BASE_URL_PATH}/reactions/POST/{post.slug}", json=reaction_dict
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "status": "success",
+        "message": "Reaction created",
+        "data": {
+            "id": mocker.ANY,
+            "user": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "rtype": reaction_dict["rtype"],
+        },
+    }
+
+
+async def test_delete_reaction(authorized_client, reaction):
+    # Test for invalid reaction id
+    response = await authorized_client.delete(
+        f"{BASE_URL_PATH}/reactions/{uuid.uuid4()}"
+    )
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Reaction does not exist",
+        "code": ErrorCode.NON_EXISTENT,
+    }
+    response = await authorized_client.delete(
+        f"{BASE_URL_PATH}/reactions/{reaction.id}"
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Reaction deleted",
+    }
+    # You can test for other error responses yourself
+
+
+async def test_retrieve_comments(client, comment):
+    user = comment.author
+    post = comment.post
+
+    # Test for invalid post slug
+    response = await client.get(f"{BASE_URL_PATH}/posts/invalid_slug/comments")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Post does not exist",
+        "code": ErrorCode.NON_EXISTENT,
+    }
+
+    # Test for valid values
+    response = await client.get(f"{BASE_URL_PATH}/posts/{post.slug}/comments")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Comments Fetched",
+        "data": {
+            "per_page": 50,
+            "current_page": 1,
+            "last_page": 1,
+            "comments": [
+                {
+                    "author": {
+                        "name": user.full_name,
+                        "username": user.username,
+                        "avatar": user.get_avatar,
+                    },
+                    "slug": comment.slug,
+                    "text": comment.text,
+                    "reactions_count": comment.reactions_count,
+                    "replies_count": comment.replies_count,
+                }
+            ],
+        },
+    }
+
+
+async def test_create_comment(authorized_client, post, mocker):
+    user = post.author
+    comment_data = {"text": "My new comment"}
+
+    response = await authorized_client.post(
+        f"{BASE_URL_PATH}/posts/{post.slug}/comments", json=comment_data
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "status": "success",
+        "message": "Comment Created",
+        "data": {
+            "author": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "slug": mocker.ANY,
+            "text": comment_data["text"],
+            "reactions_count": 0,
+            "replies_count": 0,
+        },
+    }
+    # You can test for other error responses yourself
+
+
+async def test_retrieve_comment_with_replies(client, reply):
+    user = reply.author
+    comment = reply.comment
+
+    # Test for invalid comment slug
+    response = await client.get(f"{BASE_URL_PATH}/comments/invalid_slug")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Comment does not exist",
+        "code": ErrorCode.NON_EXISTENT,
+    }
+
+    # Test for valid values
+    response = await client.get(f"{BASE_URL_PATH}/comments/{comment.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Comment and Replies Fetched",
+        "data": {
+            "comment": {
+                "author": {
+                    "name": user.full_name,
+                    "username": user.username,
+                    "avatar": user.get_avatar,
+                },
+                "slug": comment.slug,
+                "text": comment.text,
+                "reactions_count": comment.reactions_count,
+                "replies_count": 1,
+            },
+            "replies": {
+                "per_page": 50,
+                "current_page": 1,
+                "last_page": 1,
+                "items": [
+                    {
+                        "author": {
+                            "name": user.full_name,
+                            "username": user.username,
+                            "avatar": user.get_avatar,
+                        },
+                        "slug": reply.slug,
+                        "text": reply.text,
+                        "reactions_count": 0,
+                    }
+                ],
+            },
+        },
+    }
+
+
+async def test_create_reply(authorized_client, comment, mocker):
+    user = comment.author
+    reply_data = {"text": "New Cool reply"}
+
+    response = await authorized_client.post(
+        f"{BASE_URL_PATH}/comments/{comment.slug}", json=reply_data
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "status": "success",
+        "message": "Reply Created",
+        "data": {
+            "author": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "slug": mocker.ANY,
+            "text": reply_data["text"],
+            "reactions_count": 0,
+        },
+    }
+    # You can test for other error responses yourself
+
+
+async def test_update_comment(authorized_client, comment, mocker):
+    user = comment.author
+    comment_data = {"text": "New updated comment"}
+
+    response = await authorized_client.put(
+        f"{BASE_URL_PATH}/comments/{comment.slug}", json=comment_data
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Comment Updated",
+        "data": {
+            "author": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "slug": mocker.ANY,
+            "text": comment_data["text"],
+            "reactions_count": 0,
+            "replies_count": mocker.ANY,
+        },
+    }
+    # You can test for other error responses yourself
+
+
+async def test_delete_comment(authorized_client, comment):
+    response = await authorized_client.delete(
+        f"{BASE_URL_PATH}/comments/{comment.slug}"
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Comment Deleted",
+    }
+    # You can test for other error responses yourself
+
+
+async def test_retrieve_reply(client, reply):
+    user = reply.author
+
+    # Test for invalid reply slug
+    response = await client.get(f"{BASE_URL_PATH}/replies/invalid_slug")
+    assert response.status_code == 404
+    assert response.json() == {
+        "status": "failure",
+        "message": "Reply does not exist",
+        "code": ErrorCode.NON_EXISTENT,
+    }
+
+    # Test for valid values
+    response = await client.get(f"{BASE_URL_PATH}/replies/{reply.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Reply Fetched",
+        "data": {
+            "author": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "slug": reply.slug,
+            "text": reply.text,
+            "reactions_count": 0,
+        },
+    }
+
+
+async def test_update_reply(authorized_client, reply, mocker):
+    user = reply.author
+    reply_data = {"text": "New updated reply"}
+
+    response = await authorized_client.put(
+        f"{BASE_URL_PATH}/replies/{reply.slug}", json=reply_data
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Reply Updated",
+        "data": {
+            "author": {
+                "name": user.full_name,
+                "username": user.username,
+                "avatar": user.get_avatar,
+            },
+            "slug": mocker.ANY,
+            "text": reply_data["text"],
+            "reactions_count": 0,
+        },
+    }
+    # You can test for other error responses yourself
+
+
+async def test_delete_reply(authorized_client, reply):
+    response = await authorized_client.delete(f"{BASE_URL_PATH}/replies/{reply.slug}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "success",
+        "message": "Reply Deleted",
+    }
+    # You can test for other error responses yourself
